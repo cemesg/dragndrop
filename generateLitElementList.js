60752be83588bc063d0eb9aadc17d2b0a5c7d393
cemesg <cemesigok@gmail.com>
2024-07-04 10:56:15 +0300

    lit parser

---
+++ generateLitElementList.js
@@ -0,0 +1,84 @@
+const fs = require('fs');
+const path = require('path');
+const ts = require('typescript');
+
+const generateLitElementsList = (specificFolder) => {
+  const nodeModulesPath = path.resolve(__dirname, 'node_modules', specificFolder);
+  const outputFilePath = path.resolve(__dirname, 'src', 'litElementsList.json');
+
+  const litElements = [];
+
+  const processFile = (filePath) => {
+    const sourceFile = ts.createSourceFile(
+      filePath,
+      fs.readFileSync(filePath, 'utf8'),
+      ts.ScriptTarget.Latest,
+      true
+    );
+
+    const visit = (node) => {
+      if (ts.isClassDeclaration(node) && node.decorators) {
+        let extendsLitElement = false;
+        if (node.heritageClauses) {
+          node.heritageClauses.forEach(clause => {
+            if (clause.types) {
+              clause.types.forEach(type => {
+                if (type.expression && type.expression.escapedText === 'LitElement') {
+                  extendsLitElement = true;
+                }
+              });
+            }
+          });
+        }
+
+        if (extendsLitElement) {
+          const className = node.name.text;
+          const properties = [];
+
+          node.members.forEach(member => {
+            if (member.decorators) {
+              member.decorators.forEach(decorator => {
+                if (ts.isCallExpression(decorator.expression) &&
+                    ts.isIdentifier(decorator.expression.expression) &&
+                    decorator.expression.expression.text === 'property') {
+                  properties.push(member.name.text);
+                }
+              });
+            }
+          });
+
+          if (properties.length > 0) {
+            litElements.push({ className, properties });
+          }
+        }
+      }
+      ts.forEachChild(node, visit);
+    };
+
+    ts.forEachChild(sourceFile, visit);
+  };
+
+  const traverseDirectory = (dir) => {
+    fs.readdirSync(dir).forEach(file => {
+      const fullPath = path.join(dir, file);
+      if (fs.lstatSync(fullPath).isDirectory()) {
+        traverseDirectory(fullPath);
+      } else if (fullPath.endsWith('.ts') || fullPath.endsWith('.js')) {
+        processFile(fullPath);
+      }
+    });
+  };
+
+  traverseDirectory(nodeModulesPath);
+
+  fs.writeFile(outputFilePath, JSON.stringify(litElements, null, 2), (err) => {
+    if (err) {
+      console.error('Error writing litElementsList.json:', err);
+      return;
+    }
+    console.log('litElementsList.json has been saved.');
+  });
+};
+
+// Replace 'lit' with the folder name you want to parse within node_modules
+generateLitElementsList('lit');
